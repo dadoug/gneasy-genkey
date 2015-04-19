5cdc3f6e5a83850cf14409d5cf7c7304ee85eaf0
DaDoug <dadoug@riseup.net>
2015-04-18 20:45:23 -0400

    add test directory

---
+++ test/clean.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rm -rf 0x* test/dot.gnupg/*  test/egk-* test/*.log
---
+++ test/debug.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [[ ! -d test/dot.gnupg ]] ; then 
+    mkdir test/dot.gnupg
+    chmod u=rwx test/dot.gnupg
+fi
+
+./gneasy-genkey.sh "Testy McTesterson <testy@mctesterson.test>,Test Testerson <test@testerson.tst>,<@testy>" --debug
